499a5d405da0cca8b914f106a146125abd09017f
Pedro Padilha <pedropadilha@me.com>
2020-10-29 22:05:04 -0300

    Nomes dos treinos na pagina de usuario

--- routes/aluno.js
+++ routes/aluno.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Treino = require('../models/Treino');
+
 router.get('/', async (req, res) => {
+  const { _id } = req.user;
+  const treinos = await Treino.find({ aluno: _id });
+  console.log(treinos);
   return res.render('main', {
     page: 'aluno',
     path: '/aluno',
-    user: req.user
+    user: req.user,
+    treinos
   });
 });
 
